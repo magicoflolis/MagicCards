7d78817f758509cbec9651674565c6e813e47549
Magic <magicoflolis@tuta.io>
2025-08-15 14:39:25 -0700

    Uploaded build
    Signed-off-by: Magic <magicoflolis@tuta.io>

---
+++ src/Input.js
@@ -0,0 +1,16 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="es2022"/>
+
+/**
+ * Scripting API: https://help.aidungeon.com/scripting
+ * Scripting Guidebook: https://github.com/magicoflolis/aidungeon.js/blob/main/Scripting%20Guidebook.md
+ */
+
+
+input((text) => {
+  // Your Script's code goes here
+  // if (typeof text === "string") { }
+  return { text };
+});
+
+void 0
